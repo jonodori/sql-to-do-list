8f43f3422bb5934ba6bf553d4b38201d85740654
Jonathan Palalay <ring5pun@hotmail.com>
2022-05-28 15:27:45 -0500

    finished post route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,30 @@
 $(document).ready(handleReady)
 
 function handleReady(){
-    console.log('DOM/jquery loaded!');
+    $('#addBtn').on('click', addTask);
+
 }
 
 
+function addTask(){
+    //Get info to send to the server
+    const taskToSend = {
+        task: $('#tasks-id').val(),
+        details: $('#details-id').val(),
+        when: $('#date-id').val(), 
+        completed: Boolean 
+    }
+
+    console.log(taskToSend); // show that the object was created 
+
+    $.ajax({
+      type: 'POST',
+      url: '/todo',
+      data: taskToSend
+    }).then(function(response){
+        console.log(response)
+    }).catch(function(error){
+        console.log('error in todo post', error);
+        alert('Error adding task');
+    })
+};
--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,7 +4,35 @@ const todoRouter = express.Router();
 const pool = require('../modules/pool');
 
 
-//Get all tasks
+// creating the router 
+todoRouter.post('/', (req,res) => {
+    let newTask = req.body; 
+    //check to see if object got sent over 
+    console.log('Adding new task', newTask); 
+    const sqlQuery = `
+    INSERT INTO "todo" ("task", "details", "when", "completed")
+    VALUES ($1, $2, $3, $4);
+    `;
+
+    const sqlParams = [
+    
+        newTask.task,
+        newTask.details,
+        newTask.when,
+        newTask.completed
+    ];
+
+    console.log(sqlParams); 
+
+    pool.query(sqlQuery, sqlParams)
+        .then(() => {
+            res.sendStatus(201);
+        })
+        .catch((err) => {
+            console.log(`POST to db failed: ${err}`);
+            res.sendStatus(500);
+        });
+});
 
 
 module.exports = todoRouter;
