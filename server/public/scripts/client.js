2d5466e677bae060918805e08439380ff263c46e
Jonathan Palalay <ring5pun@hotmail.com>
2022-05-28 16:39:28 -0500

    finished creating my get route and appended to the DOM

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,7 +2,7 @@ $(document).ready(handleReady)
 
 function handleReady(){
     $('#addBtn').on('click', addTask);
-
+    getTask(); // shows tasks 
 }
 
 
@@ -26,5 +26,45 @@ function addTask(){
     }).catch(function(error){
         console.log('error in todo post', error);
         alert('Error adding task');
-    })
+    });
+
+    getTask(); //runs the get 
+
+    // $('.clear').val(''); // clear inputs
 };
+
+function getTask(){
+    $.ajax({
+        url: '/todo',
+        method: 'GET'
+    }).then((res) => {
+        console.log('Receive data back');
+        displayTask(res);
+    }).catch(() => {
+        console.log('Failed to get data back')
+    })
+}
+
+function displayTask(tasks){
+   
+
+    for (task of tasks) {
+        let date = new Date(task.when)
+
+        $('#viewTasks').append(`
+        <tr>
+          <td>${task.task}</td>
+          <td>${task.details}</td>
+          <td>${date.toLocaleDateString()}</td>
+          <td>${task.completed}</td>
+          <td>
+          <button class ="markCompleteBtn">Complete</button>
+          </td>
+          <td>
+          <button class ="deleteBtn">Delete</button>
+          </td>
+      </tr>
+        `);
+      }
+
+}
